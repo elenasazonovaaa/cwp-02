a763a234a037e0b83de9ff88140f4252ef9a7e84
elenasazonovaaa <elenasazonovaaa@gmail.com>
2017-09-25 15:47:27 +0300

    message QA-ASC-DSC

--- client.js
+++ client.js
@@ -7,12 +7,29 @@ client.setEncoding('utf8');
 
 client.connect(port, function() {
     console.log('Connected');
-    client.write('Hello, Server!');
+    client.write('QA');
 });
 
-client.on('data', function(data) {
-    console.log(data);
-    client.destroy();
+client.on('data', function(data,err) {
+    if(err) console.log('Error in get data');
+    else
+    {
+        if(data === 'ASC')
+        {
+            console.log('WOW!Server ASC!');
+            client.destroy();
+        }
+        else if(data === 'DSC')
+        {
+            console.log('Server DSC!');
+            client.destroy();
+        }
+        else
+        {
+            console.log(':( I do not understand, Server!');
+            client.destroy();
+        }
+    }
 });
 
 client.on('close', function() {
--- server.js
+++ server.js
@@ -5,16 +5,24 @@ var seed = 0;
 const server = net.createServer(function(client){
 
 client.setEncoding('utf8');
-client.id = Date.now() + seed++;
 
-console.log(' +++ '+'Client-'+ client.id);
-
-client.on('data', (data) => {
-    console.log(data);
-client.write('Hello - '+client.id);
+client.on('data', function(data,err){
+   if(err) console.log(`Error in get data`);
+   else
+   {
+       if(data === 'QA')
+       {
+           client.id = Date.now() + seed++;
+           console.log(' +++ '+'Client-'+ client.id);
+           client.write('ASC');
+       }
+       else client.write('DSC');
+   }
 });
 
-client.on('end', () => console.log(' --- '+'Client-' + client.id));
+client.on('end', function(){
+    if(client.id === undefined) console.log(' --- '+'no connect for Client');
+    else console.log(' --- '+'Client-' + client.id);} );
 });
 server.listen(port, () => {
     console.log(`Server listening on localhost:${port}`);
